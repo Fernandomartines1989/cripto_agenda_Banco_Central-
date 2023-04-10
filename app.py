2ea3d768e20f9b68981208a71922371a22f7f776
Fernando Martines <114265995+Fernandomartines1989@users.noreply.github.com>
2023-04-09 22:34:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+import os
+import requests
+import getpass
+import json
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+
 from flask import Flask
+
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY_2"]
+TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID_2"]
 
 app = Flask(__name__)
 
