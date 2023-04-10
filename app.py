b38a3485b9ce901131a170b045a6e2d38f3681ed
Fernando Martines <114265995+Fernandomartines1989@users.noreply.github.com>
2023-04-09 22:39:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ from flask import Flask
 
 import re
 
+import httpx
+
+hoje = datetime.now().strftime("%Y-%m-%d")
+data = httpx.get(f"https://www.bcb.gov.br/api/servico/sitebcb/agendadiretoria?lista=Agenda%20da%20Diretoria&inicioAgenda=%27{hoje}%27&fimAgenda=%27{hoje}%27")
+
+dados = data.json()
+
+desc = dados["conteudo"][0]["descricao"]
+
+parsed = BeautifulSoup(desc, "html.parser")
+
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY_2"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID_2"]
 
