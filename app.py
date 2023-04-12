413986438fcebf1989beb034bbe288328a9d129a
Fernando Martines <114265995+Fernandomartines1989@users.noreply.github.com>
2023-04-12 18:56:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,25 +57,17 @@ def hello_world():
 
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
+  hoje = datetime.now().strftime("%d-%m-%Y")
   update = request.json
   chat_id = update["message"]["chat"]["id"]
   message = update["message"]["text"]
-  
+  nova_mensagem = {"chat_id": chat_id, "text": raspagem_bacen}
+  mensagem_if = {"chat_id": chat_id, "text": f"Olá! Seja bem-vindo(a). Quer saber se alguma autoridade do Banco Central tem algum evento de criptomoedas em sua agenda em {hoje}? Digite Sim" }
+  mensagem_else = {"chat_id": chat_id, "text": "Não entendi Digite /start e eu te digo o que sei fazer" }
   if message == "/start":
-      hoje = datetime.now().strftime("%d-%m-%Y")
-      mensagem = f"Olá! Seja bem-vindo(a). Quer saber se alguma autoridade do Banco Central tem algum evento de criptomoedas em sua agenda em {hoje}? Digite Sim"
-                                     
+    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_if)
   elif message.lower() == "sim":
-      mensagem = raspagem_bacen
-                                  
+    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
   else:
-      mensagem = "Não entendi. Digite /start e eu te digo o que eu sei fazer"
-                                     
-  requests.post(
-      f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage",
-      chat_id=chat_id,
-      data=mensagem,
-  )
-       
+    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_else)
   return "ok"
-
