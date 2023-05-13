bf9c12cb6230622531a12eb8cf97292daf263b1e
Fernando Martines <114265995+Fernandomartines1989@users.noreply.github.com>
2023-05-12 21:52:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
 import os
-import requests
-import getpass
-import json
-
-from bs4 import BeautifulSoup
-from datetime import datetime 
-
-from flask import Flask, request
-
-import re
+from datetime import datetime
 
 import httpx
+from bs4 import BeautifulSoup
+from flask import Flask, request
 
 hoje = datetime.now().strftime("%Y-%m-%d")
 data = httpx.get(f"https://www.bcb.gov.br/api/servico/sitebcb/agendadiretoria?lista=Agenda%20da%20Diretoria&inicioAgenda=%27{hoje}%27&fimAgenda=%27{hoje}%27")
-
 dados = data.json()
 
 desc = dados["conteudo"][0]["descricao"]
-
 parsed = BeautifulSoup(desc, "html.parser")
 
 mensagens = []
 
+if not dados["conteudo"]:
+    mensagens.append("Hoje não tem nenhum tipo de evento na agenda de autoridades")
+else:
+    for compromisso in dados["conteudo"]:
+        autoridade = compromisso.get("autoridade")
+        descricao = compromisso.get("descricao")
+        if not descricao:
+            continue
+        html = BeautifulSoup(compromisso.get("descricao"))
+        descricao = html.text
+        printado = False
+        for word in ['btc', 'cripto', "bitcoin", "criptomoeda", "real digital", "CBDC", "ethereum", "mercado bitcoin", "binance", "bitso", "blockchain", "real tokenizado", "cripto"]:
+            if not printado and word.lower() in compromisso['descricao'].lower():
+                mensagens.append(f"A autoridade {autoridade} tem um compromisso de autoridade do BC no setor cripto hoje: {descricao}")
+                mensagem_adicionada = True
+                printado = True
 
-for compromisso in dados["conteudo"]:
-    autoridade = compromisso.get("autoridade") 
-    descricao = compromisso.get("descricao")
-    if not descricao:
-      continue
-    html = BeautifulSoup(compromisso.get("descricao"))
-    descricao = html.text 
-    printado = False
-    for word in ['btc', 'cripto', "bitcoin", "criptomoeda", "real digital", "CBDC", "ethereum", "mercado bitcoin", "binance", "bitso", "blockchain", "real tokenizado", "cripto"]:
-        if not printado and word.lower() in compromisso['descricao'].lower():
-          mensagens.append(f"A autoridade {autoridade} tem um compromisso de autoridade do BC no setor cripto hoje: {descricao}")
-          mensagem_adicionada = True
-          printado = True
-        
-if not mensagens:
-    mensagens.append("Hoje nenhuma autoridade da Banco Central tem na agenda um evento envolvendo criptomoedas")
-
+    if not mensagens:
+        mensagens.append("Hoje nenhuma autoridade do Banco Central tem na agenda um evento envolvendo criptomoedas")
 
 raspagem_bacen = '\n'.join(mensagens)
 
@@ -50,26 +42,24 @@ TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID_22"]
 app = Flask(__name__)
 
 
-
-
 @app.route("/")
 def hello_world():
-   return "Olá, mundo!"
+    return "Olá, mundo!"
 
 
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
-  hoje = datetime.now().strftime("%d-%m-%Y")
-  update = request.json
-  chat_id = update["message"]["chat"]["id"]
-  message = update["message"]["text"]
-  nova_mensagem = {"chat_id": chat_id, "text": raspagem_bacen}
-  mensagem_if = {"chat_id": chat_id, "text": f"Olá! Seja bem-vindo(a). Quer saber se alguma autoridade do Banco Central tem algum evento de criptomoedas em sua agenda em {hoje}? Digite Sim" }
-  mensagem_else = {"chat_id": chat_id, "text": "Não entendi Digite /start e eu te digo o que sei fazer" }
-  if message == "/start":
-    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_if)
-  elif message.lower() == "sim":
-    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  else:
-    texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_else)
-  return "ok"
+    hoje = datetime.now().strftime("%d-%m-%Y")
+    update = request.json
+    chat_id = update["message"]["chat"]["id"]
+    message = update["message"]["text"]
+    nova_mensagem = {"chat_id": chat_id, "text": raspagem_bacen}
+    mensagem_if = {"chat_id": chat_id, "text": f"Olá! Seja bem-vindo(a). Quer saber se alguma autoridade do Banco Central tem algum evento de criptomoedas em sua agenda em {hoje}? Digite Sim"}
+    mensagem_else = {"chat_id": chat_id, "text": "Não entendi Digite /start e eu te digo o que sei fazer"}
+    if message == "/start":
+        texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_if)
+    elif message.lower() == "sim":
+        texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+    else:
+        texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_else)
+    return "ok"
